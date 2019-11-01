b2ef11ece36e33505135a905ffb9e1c316419929
Aigerim260992 <djandullaeva.aigerim@gmail.com>
2019-11-01 20:17:34 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.logging.SocketHandler;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Aidar", "Nurgazy", "Adilhan"};
+
+        for (String name : names) {
+            switch (name) {
+                case "Aidar":
+                    System.out.println(name + " Доброе утро!");
+                    break;
+                case "Nurgazy":
+                    System.out.println(name + " Доброго дня!");
+                    break;
+                case "Adilhan":
+                    System.out.println(name + " Добрый вечер!");
+                    break;
+
+
+            }
+        }
+    }
+}
